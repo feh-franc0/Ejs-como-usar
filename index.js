79a618a24d988de7325919145d143dcddaf61186
feh-franc0 <fernandofv1110@gmail.com>
2022-04-04 23:13:11 -0300

    Substituição de variáveis

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+let user = {
+    name: "Igor",
+    phone: "(111)1233445"
+}
+
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
 
-    res.render('user')
+    res.render('user', user);
 
 })
 
